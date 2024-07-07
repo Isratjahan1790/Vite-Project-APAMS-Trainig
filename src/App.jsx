d4cc5fa044908a7c289fc464aba9cfd2679761e1
shamuel.molla <shamuel.molla@businessaccelerate.com.bd>
2024-07-07 17:07:40 +0600

    updated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import LoginForm from "./Component/Form/Form";
+
+function App() {
+
+  return (
+<div>
+<LoginForm />
+</div>
+  )
+}
+
+export default App
---
+++ src/Component/Form/Form.jsx
@@ -0,0 +1,14 @@
+import {Form} from 'react-bootstrap';
+
+function LoginForm (){
+
+    return (
+      <div>
+        <Form>
+          <h1 className='text-center text-danger'>This is Form</h1>
+        </Form>
+      </div>
+    );
+}
+
+export default LoginForm;
